38142cdb3cb23d46200bcf8c33517e8347cf8cad
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-09-20 15:08:35 -0700

    betty.c

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a README.md file for the 0x16. C - Simple Shell project
---
+++ betty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints "Hello, Betty!" to the standard output.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("Hello, Betty!\n");
+	return (0);
+}
